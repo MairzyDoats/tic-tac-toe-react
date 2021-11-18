2b137f7f409e5f5302db7f614fa5d91e2928149b
MairzyDoats <jk101@gmx.de>
2021-11-18 19:01:42 +0100

    delete unused var

--- src/components/ResetButton.js
+++ src/components/ResetButton.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { GameContext } from './Game'
 
 export default function ResetButton({ text }) {
-  const { handleReset, reset } = useContext(GameContext)
+  const { handleReset } = useContext(GameContext)
   return (
     <button className="reset__button" onClick={handleReset}>{text}</button>
   )
