9d62525682df4342475fba4dcfb5f9f846e19675
MairzyDoats <jk101@gmx.de>
2021-11-18 18:35:07 +0100

    Add footer and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './css/App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="header__heading">Tic <span className="header__heading--contrast">Tac</span> Toe</h1>
+      <hr className="header__hr" />
+    </div>
+  )
+}
